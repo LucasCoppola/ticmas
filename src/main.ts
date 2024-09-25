0685c19f142bd4446f6aa4e5187c96adf3505b76
LucasCoppola <lucascoppola21@gmail.com>
2024-09-25 12:57:15 -0300

    health check route

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RequestMethod, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: 'healthz', method: RequestMethod.GET }],
+  });
+  await app.listen(8080);
 }
 bootstrap();
